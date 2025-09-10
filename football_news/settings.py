2c0273e3719f47fb260ef949b56ce841cc08f525
farishuda <farishudaku@gmail.com>
2025-09-10 15:01:04 +0700

    Perubahan pada CSRF

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ DEBUG = True
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "faris-huda-footballnews.pbp.cs.ui.ac.id"]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://faris-huda-footballnews.pbp.cs.ui.ac.id/"
+    "https://faris-huda-footballnews.pbp.cs.ui.ac.id"
 ]
 
 # Application definition
