08dbe60981e4212c1632ee9cbdee7fac6311a174
farishuda <farishudaku@gmail.com>
2025-10-01 14:11:08 +0700

    Perbaikan bug

--- main/forms.py
+++ main/forms.py
@@ -7,10 +7,10 @@ class NewsForm(ModelForm):
         model = News
         fields = ["title", "content", "category", "thumbnail", "is_featured"]
 
-        def clean_title(self):
-            title = self.cleaned_data["title"]
-            return strip_tags(title)
+    def clean_title(self):
+        title = self.cleaned_data["title"]
+        return strip_tags(title)
 
-        def clean_content(self):
-            content = self.cleaned_data["content"]
-            return strip_tags(content)
+    def clean_content(self):
+        content = self.cleaned_data["content"]
+        return strip_tags(content)
