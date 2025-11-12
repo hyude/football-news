a4c7ca5610ae2fd44afd2c89715e5c2a24027cfd
farishuda <farishudaku@gmail.com>
2025-11-12 14:51:23 +0700

    Tutorial 8

--- football_news/settings.py
+++ football_news/settings.py
@@ -30,7 +30,15 @@ SECRET_KEY = 'django-insecure-lcy-i@^x3no6=s&^rw06a*oy=zbtfws+$7m(@dt0_lrhr5e#w)
 PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "faris-huda-footballnews.pbp.cs.ui.ac.id"]
+# CORS HEADERS
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "faris-huda-footballnews.pbp.cs.ui.ac.id", "10.0.2.2"]
 
 CSRF_TRUSTED_ORIGINS = [
     "https://faris-huda-footballnews.pbp.cs.ui.ac.id"
@@ -45,7 +53,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main'
+    'main',
+    'authentication',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -57,6 +67,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'football_news.urls'
